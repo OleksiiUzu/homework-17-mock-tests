b7b01c954306a117cafbaf035b19026bdc9d2508
Oleksii <jackinsonalex@gmail.com>
2023-09-30 08:39:46 +0300

    added mock for main.py

--- MockTests.py
+++ MockTests.py
@@ -3,6 +3,7 @@ from unittest.mock import Mock, patch, ANY
 import pokemon_name_translator
 from pokemon_report import PokemonReport
 from pokemon_service import PokemonService
+from main import main
 
 
 class TestMock(unittest.TestCase):
@@ -53,3 +54,23 @@ class TestMock(unittest.TestCase):
         mock_get.assert_called_once_with(f"{service.BASE_URL}/{pokemon_name}")
 
         self.assertEqual(result, response_json)
+
+    @patch('main.PokemonService')
+    @patch('main.PokemonNameTranslator')
+    @patch('main.PokemonReport')
+    def test_main(self, mock_report, mock_translator, mock_service):
+        mock_service_instance = mock_service.return_value
+        mock_translator_instance = mock_translator.return_value
+        mock_report_instance = mock_report.return_value
+
+        pokemon_info = {'name': 'Pikachu', 'height': 40, 'weight': 60}
+        mock_service_instance.get_pokemon_info.return_value = pokemon_info
+
+        mock_translator_instance.translate.return_value = "Pikachu (French)"
+
+        main()
+
+        mock_service_instance.get_pokemon_info.assert_called_once_with("pikachu")
+        mock_translator_instance.translate.assert_called_once_with("pikachu", target_language="fr")
+        mock_report_instance.generate_report.assert_called_once_with(pokemon_info, "Pikachu (French)",
+                                                                     "pokemon_report.pdf")
